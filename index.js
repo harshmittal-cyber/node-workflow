0700623ae5d60def345192c154d87d728480c3d8
Harsh Mittal <harshmittal-cyber>
2023-05-10 19:01:59 +0530

    multipke job

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.listen(3000, (req, res) => {
     console.log('hi harsh we are running github action workflow in nodejs');
     return res.status(200).json({
-        message: `Server is running on port ${PORT}`,
+        message: `Server is running on port 3000`,
         success: true
     })
 })
