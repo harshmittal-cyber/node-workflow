df05e390035aad705978b5fc49144f72d39da99e
Harsh Mittal <harshmittal-cyber>
2023-05-10 21:27:17 +0530

    change

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ const express = require('express');
 const app = express();
 
 
-
 app.listen(3000, (req, res) => {
-    console.log('hi harsh we are running github action workflow in nodejs');
-    return res.status(200).json({
-        message: `Server is running on port 3000`,
-        success: true
-    })
+    console.log('hi harsh we are running github action workflow in nodejs on port 3000');
 })
